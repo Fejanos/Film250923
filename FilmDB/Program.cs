d2818405bf43db3177ab785a5b75641620824024
Fejanos <fenyveijani97@gmail.com>
2025-09-23 13:51:01 +0200

    1. struktura

--- FilmDB/Program.cs
+++ FilmDB/Program.cs
@@ -8,8 +8,22 @@ namespace FilmDB
 {
     internal class Program
     {
+        struct Film
+        {
+            public string Cim;
+            public string Rendezo;
+            public int Ev;
+            public int Hossz_perc;
+        }
         static void Main(string[] args)
         {
+            Film f = new Film();
+            f.Cim = "Feláldozhatók";
+            f.Rendezo = "Sylvester Stallone";
+            f.Ev = 2010;
+            f.Hossz_perc = 103;
+
+            Console.WriteLine(f.Cim);
         }
     }
 }
