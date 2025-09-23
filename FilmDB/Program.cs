57e45cb768200477689456b37317c805395f5fb5
Domjan Dominika <domjan.dominika@simonyiszki.org>
2025-09-23 13:55:01 +0200

    A halott menyasszony hozzáadva

--- FilmDB/Program.cs
+++ FilmDB/Program.cs
@@ -8,8 +8,29 @@ namespace FilmDB
 {
     internal class Program
     {
+        struct Film
+        {
+            public string cim;
+            public string rendezo;
+            public int ev;
+            public int hossz_p;
+        }
         static void Main(string[] args)
         {
+            Film f = new Film();
+            f.cim = "Feláldozhatók";
+            f.rendezo = "Sylvester Stallone";
+            f.ev = 2010;
+            f.hossz_p = 103;
+
+            Film m = new Film();
+            f.cim = "A halott menyasszony";
+            f.rendezo = "Tim Burton";
+            f.ev = 2005;
+            f.hossz_p = 77;
+
+            Console.ReadKey();
         }
+        
     }
 }
